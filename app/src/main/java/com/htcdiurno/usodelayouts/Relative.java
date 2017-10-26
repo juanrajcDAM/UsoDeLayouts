41ffa2587de78501ac6941b47f3642afa903958b
juanrajc <juanrajc@hotmail.com>
2017-10-26 10:23:43 +0200

    Relative hecho.

---
+++ app/src/main/java/com/htcdiurno/usodelayouts/Relative.java
@@ -0,0 +1,17 @@
+package com.htcdiurno.usodelayouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by juanrajc on 26/10/2017.
+ */
+
+public class Relative extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative);
+    }
+}
